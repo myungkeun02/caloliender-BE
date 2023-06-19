498ae5d5f536c26c2b7c396fc8951928de1a0cc6
myungkeun <audrms3568@gmail.com>
2023-06-20 00:13:53 +0900

    feat: google 소셜로그인 구현

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
+import { UserModule } from './user/user.module';
+import { DiaryModule } from './diary/diary.module';
+import { AuthModule } from './auth/auth.module';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, UserModule, DiaryModule],
+  controllers: [AppController, AuthController, UserController],
+  providers: [AppService, UserService, AuthService],
 })
 export class AppModule {}
